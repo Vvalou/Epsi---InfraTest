2d8bab48a59597a6d62e45a23469795aa2c9da71
Vvalou <vergult.valentin34@gmail.com>
2025-11-03 13:47:09 +0100

    Création du getArea

--- src/app.js
+++ src/app.js
@@ -9,6 +9,18 @@ class Triangle {
     getSides() {
         return [this.a, this.b, this.c];
     }
+
+    // Calcule le périmètre : somme des 3 côtés
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    // Calcule l’aire avec la formule de Héron
+    getArea() {
+        const s = (this.a + this.b + this.c) / 2;
+        const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+        return area;
+    }
 }
 
 module.exports = {
